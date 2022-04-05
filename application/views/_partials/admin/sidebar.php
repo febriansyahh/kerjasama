c2324c05d4eeb98b72087299eaf48a5bee838bcf
febriansyahh <syahbanafebrian41@gmail.com>
2022-04-05 13:29:26 +0700

    up

--- application/views/_partials/admin/sidebar.php
+++ application/views/_partials/admin/sidebar.php
@@ -58,24 +58,24 @@
           role="list" id="submenu-app" aria-expanded="false">
           <ul class="flex-column nav">
             <li class="nav-item ">
-              <a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
+              <li><a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
                 <span class="sidebar-text">Data Unit</span>
-              </a>
+              </a></li>
             </li>
             <li class="nav-item ">
-              <a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
+              <li><a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
                 <span class="sidebar-text">Data MoU</span>
-              </a>
+              </a></li>
             </li>
             <li class="nav-item ">
-              <a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
-                <span class="sidebar-text">Data PKS</span>
-              </a>
+              <li><a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
+                <span class="sidebar-text">Data Kerjasama</span>
+              </a></li>
             </li>
             <li class="nav-item ">
-              <a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
+              <li><a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
                 <span class="sidebar-text">Data IA</span>
-              </a>
+              </a></li>
             </li>
           </ul>
         </div>
@@ -83,177 +83,41 @@
       <li class="nav-item ">
         <a href="../../pages/transactions.html" class="nav-link">
           <span class="sidebar-icon">
-            <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path><path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd"></path></svg>
+             <i class="fas fa-file"></i>
           </span>
-          <span class="sidebar-text">Master Unit</span>
+          <span class="sidebar-text">Usulan Kerjasama</span>
         </a>
       </li>
+
       <li class="nav-item ">
-        <a href="../../pages/settings.html" class="nav-link">
+        <a href="../../pages/transactions.html" class="nav-link">
           <span class="sidebar-icon">
-            <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
+             <i class="fas fa-file"></i>
           </span>
-          <span class="sidebar-text">Settings</span>
+          <span class="sidebar-text">Ajuan IA</span>
         </a>
       </li>
-      <li class="nav-item">
-        <a href="https://demo.themesberg.com/volt-pro/pages/calendar.html" target="_blank" class="nav-link d-flex justify-content-between">
-          <span>
-            <span class="sidebar-icon">
-              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clip-rule="evenodd"></path></svg>
-            </span>
-            <span class="sidebar-text">Calendar</span>
+      
+      <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
+      
+      <li class="nav-item ">
+        <a href="../../pages/transactions.html" class="nav-link">
+          <span class="sidebar-icon">
+          <i class="fas fa-layer-group"></i>
           </span>
-          <span>
-            <span class="badge badge-sm bg-secondary ms-1 text-gray-800">Pro</span>
-          </span>
+          <span class="sidebar-text">Master Tingkatan</span>
         </a>
       </li>
-      <li class="nav-item">
-        <a href="https://demo.themesberg.com/volt-pro/pages/map.html" target="_blank" class="nav-link d-flex justify-content-between">
-          <span>
-            <span class="sidebar-icon">
-              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-            </span>
-            <span class="sidebar-text">Map</span>
+
+      <li class="nav-item ">
+        <a href="../../pages/transactions.html" class="nav-link">
+          <span class="sidebar-icon">
+          <i class="fas fa-users"></i>
           </span>
-          <span>
-            <span class="badge badge-sm bg-secondary ms-1 text-gray-800">Pro</span>
-          </span>
+          <span class="sidebar-text">Data User</span>
         </a>
       </li>
-      <li class="nav-item">
-        <span
-          class="nav-link  collapsed  d-flex justify-content-between align-items-center"
-          data-bs-toggle="collapse" data-bs-target="#submenu-app">
-          <span>
-            <span class="sidebar-icon">
-              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
-            </span> 
-            <span class="sidebar-text">Tables</span>
-          </span>
-          <span class="link-arrow">
-            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </span>
-        </span>
-        <div class="multi-level collapse "
-          role="list" id="submenu-app" aria-expanded="false">
-          <ul class="flex-column nav">
-            <li class="nav-item ">
-              <a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
-                <span class="sidebar-text">Bootstrap Tables</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="nav-item">
-        <span
-          class="nav-link  collapsed  d-flex justify-content-between align-items-center"
-          data-bs-toggle="collapse" data-bs-target="#submenu-pages">
-          <span>
-            <span class="sidebar-icon">
-              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-            </span> 
-            <span class="sidebar-text">Page examples</span>
-          </span>
-          <span class="link-arrow">
-            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </span>
-        </span>
-        <div class="multi-level collapse " role="list"
-          id="submenu-pages" aria-expanded="false">
-          <ul class="flex-column nav">
-            <li class="nav-item">
-              <a class="nav-link" href="../../pages/examples/sign-in.html">
-                <span class="sidebar-text">Sign In</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../../pages/examples/sign-up.html">
-                <span class="sidebar-text">Sign Up</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../../pages/examples/forgot-password.html">
-                <span class="sidebar-text">Forgot password</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../../pages/examples/reset-password.html">
-                <span class="sidebar-text">Reset password</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../../pages/examples/lock.html">
-                <span class="sidebar-text">Lock</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../../pages/examples/404.html">
-                <span class="sidebar-text">404 Not Found</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../../pages/examples/500.html">
-                <span class="sidebar-text">500 Not Found</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="nav-item">
-        <span
-          class="nav-link  collapsed  d-flex justify-content-between align-items-center"
-          data-bs-toggle="collapse" data-bs-target="#submenu-components">
-          <span>
-            <span class="sidebar-icon">
-              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"></path><path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            </span> 
-            <span class="sidebar-text">Components</span>
-          </span>
-          <span class="link-arrow">
-            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </span>
-        </span>
-        <div class="multi-level collapse " role="list"
-          id="submenu-components" aria-expanded="false">
-          <ul class="flex-column nav">
-            <li class="nav-item">
-              <a class="nav-link" target="_blank"
-                href="https://themesberg.com/docs/volt-bootstrap-5-dashboard/components/accordions/">
-                <span class="sidebar-text">All Components</span>
-              </a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="../../pages/components/buttons.html">
-                <span class="sidebar-text">Buttons</span>
-              </a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="../../pages/components/notifications.html">
-                <span class="sidebar-text">Notifications</span>
-              </a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="../../pages/components/forms.html">
-                <span class="sidebar-text">Forms</span>
-              </a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="../../pages/components/modals.html">
-                <span class="sidebar-text">Modals</span>
-              </a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="../../pages/components/typography.html">
-                <span class="sidebar-text">Typography</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </li>
-    
+
       <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
       <li class="nav-item ">
         <a href="<?php echo site_url('home')?>" class="nav-link">
