03f49cfd0c62166f22382e93386efcfd8cba3a7f
febriansyahh <syahbanafebrian41@gmail.com>
2022-04-05 11:58:03 +0700

    up

--- application/views/_partials/admin/sidebar.php
+++ application/views/_partials/admin/sidebar.php
@@ -34,8 +34,10 @@
       <li class="nav-item  active ">
         <a href="../../pages/dashboard/dashboard.html" class="nav-link">
          <!-- <i class="fas fa-home"></i> -->
-         <i class="fas fa-clock fa-lg"></i>
-          <span class="sidebar-text">Dashboard</span>
+         <span class="sidebar-icon">
+         <i class="fas fa-home"></i>
+         </span>
+         <span class="sidebar-text"> Dashboard</span>
         </a>
       </li>
       <li class="nav-item">
@@ -44,7 +46,7 @@
           data-bs-toggle="collapse" data-bs-target="#submenu-app">
           <span>
             <span class="sidebar-icon">
-              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
+              <i class="fas fa-database"></i>
             </span> 
             <span class="sidebar-text">Master Data</span>
           </span>
