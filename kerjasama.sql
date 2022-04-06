f2a563adf7a9a39bd90b59739f1aa02c89f74e18
febriansyahh <syahbanafebrian41@gmail.com>
2022-04-06 07:47:33 +0700

    up

---
+++ kerjasama.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2022 at 02:47 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kerjasama`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_unit`
+--
+
+CREATE TABLE `mst_unit` (
+  `idUnit` int(11) NOT NULL,
+  `nmUnit` varchar(200) NOT NULL,
+  `idTingkat` int(11) NOT NULL,
+  `sysInput` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_unit`
+--
+
+INSERT INTO `mst_unit` (`idUnit`, `nmUnit`, `idTingkat`, `sysInput`) VALUES
+(1, 'UPT-PSI', 3, '2022-04-05 16:04:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tingkatan`
+--
+
+CREATE TABLE `tingkatan` (
+  `idTingkatan` int(11) NOT NULL,
+  `nmTingkatan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tingkatan`
+--
+
+INSERT INTO `tingkatan` (`idTingkatan`, `nmTingkatan`) VALUES
+(1, 'Program Studi'),
+(3, 'Unit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL,
+  `nmUser` varchar(200) NOT NULL,
+  `username` varchar(35) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `idUnit` int(11) NOT NULL,
+  `levelUser` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mst_unit`
+--
+ALTER TABLE `mst_unit`
+  ADD PRIMARY KEY (`idUnit`);
+
+--
+-- Indexes for table `tingkatan`
+--
+ALTER TABLE `tingkatan`
+  ADD PRIMARY KEY (`idTingkatan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mst_unit`
+--
+ALTER TABLE `mst_unit`
+  MODIFY `idUnit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tingkatan`
+--
+ALTER TABLE `tingkatan`
+  MODIFY `idTingkatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
