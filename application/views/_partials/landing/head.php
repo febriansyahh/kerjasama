40613d35317d3a2c38082bcc828f2589240dff8a
febriansyahh <syahbanafebrian41@gmail.com>
2022-04-13 13:26:07 +0700

    up

--- application/views/_partials/landing/head.php
+++ application/views/_partials/landing/head.php
@@ -14,8 +14,8 @@
 <!-- ===============================================-->
 <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
 <link href=<?php echo base_url('assets/img/umkicon.png') ?> rel="icon">
-<link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+<!-- <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png"> -->
+<!-- <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png"> -->
 <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url('assets/img/favicons/logo-umk.png') ?>">
 <link rel="manifest" href="<?php echo base_url('assets/img/favicons/manifest.json') ?>">
 <meta name="msapplication-TileImage" content="<?php echo base_url('assets/img/favicons/mstile-150x150.png') ?>">
