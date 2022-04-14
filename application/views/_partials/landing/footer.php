9fb51cb7b5273a04c32dcdafa2a8f7fe9c59c494
febriansyahh <syahbanafebrian41@gmail.com>
2022-04-14 08:06:25 +0700

    up

--- application/views/_partials/landing/footer.php
+++ application/views/_partials/landing/footer.php
@@ -1,31 +1,31 @@
 <div class="container">
     <div class="row border-top border-top-secondary pt-7">
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-6 mb-lg-0 mb-sm-2 order-1 order-md-1 order-lg-1"><img class="mb-4" src="assets/img/logo-sarpras.png" width="184" alt="" /></div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-6 mb-lg-0 mb-sm-2 order-1 order-md-1 order-lg-1"><img class="mb-4" src="assets/img/logo-kerjasama-new.png" width="184" alt="" /></div>
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 order-3 order-md-3 order-lg-2">
             <p class="fs-2 mb-lg-4">Quick Links</p>
             <ul class="list-unstyled mb-0">
-                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">About us</a></li>
+                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="https://umk.ac.id/" target="_blank">Web UMK</a></li>
                 <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Blog</a></li>
-                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Contact</a></li>
-                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">FAQ</a></li>
             </ul>
         </div>
-        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 order-4 order-md-4 order-lg-3">
+        <div class="col-lg-6 col-md-6 mb-4 mb-lg-0 order-4 order-md-4 order-lg-3">
             <p class="fs-2 mb-lg-4">Legal stuff</p>
-            <ul class="list-unstyled mb-0">
-                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Disclaimer</a></li>
-                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Financing</a></li>
-                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Privacy Policy</a></li>
-                <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Terms of Service</a></li>
-            </ul>
+            <p style="font-family: Poppins">Gondangmanis PO. BOX 53 Bae 59324 Kudus <br>
+                Jawa Tengah - Indonesia <br>
+                Telp. +62291-438229 Fax. +62291-437198 <br>
+                email. <a href="mailto:perpustakaan@umk.ac.id">perpustakaan@umk.ac.id</a>
+            </p>
+            <a href="https://www.facebook.com/UNIVERSITAS-MURIA-KUDUS-UMK-182450701824294/" target="_blank" class="facebook"><i class="fab fa-facebook-square fa-xl"></i></a>
+            <a href="https://twitter.com/@UMK_mu" target="_blank" class="twitter"><i class="fab fa-twitter-square fa-xl"></i></a>
+            <a href="https://www.instagram.com/muriakudusuniversity/" target="_blank" class="instagram"><i class="fab fa-instagram-square fa-xl"></i></a>
+            <a href="https://www.youtube.com/user/channelUMK" target="_blank" class="google-plus"><i class="fab fa-youtube-square fa-xl"></i></a>
         </div>
-        <div class="col-lg-3 col-md-6 col-6 mb-4 mb-lg-0 order-2 order-md-2 order-lg-4">
-            <p class="fs-2 mb-lg-4">
-                knowing you're always on the best energy deal.</p>
-            <form class="mb-3">
-                <input class="form-control" type="email" placeholder="Enter your phone Number" aria-label="phone" />
-            </form>
-            <button class="btn btn-warning fw-medium py-1">Sign up Now</button>
-        </div>
+        <!-- <div class="col-lg-3 col-md-6 col-6 mb-4 mb-lg-0 order-2 order-md-2 order-lg-4">
+            <p class="fs-2 mb-lg-4">Social Media</p>
+            <a href="https://www.facebook.com/UNIVERSITAS-MURIA-KUDUS-UMK-182450701824294/" target="_blank" class="facebook"><i class="fab fa-facebook-square fa-xl"></i></a>
+            <a href="https://twitter.com/@UMK_mu" target="_blank" class="twitter"><i class="fab fa-twitter-square fa-xl"></i></a>
+            <a href="https://www.instagram.com/muriakudusuniversity/" target="_blank" class="instagram"><i class="fab fa-instagram-square fa-xl"></i></a>
+            <a href="https://www.youtube.com/user/channelUMK" target="_blank" class="google-plus"><i class="fab fa-youtube-square fa-xl"></i></a>
+        </div> -->
     </div>
 </div>
