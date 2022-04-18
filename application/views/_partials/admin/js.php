8973b3b9c62bb4de1ba167e2b5cd08aaf100bc49
febriansyahh <syahbanafebrian41@gmail.com>
2022-04-18 12:06:35 +0700

    up

--- application/views/_partials/admin/js.php
+++ application/views/_partials/admin/js.php
@@ -1,42 +1,42 @@
-<script src="<?php echo base_url('vendor/@popperjs/core/dist/umd/popper.min.js');?>"></script>
-<script src="<?php echo base_url('vendor/bootstrap/dist/js/bootstrap.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/@popperjs/core/dist/umd/popper.min.js'); ?>"></script>
+<script src="<?php echo base_url('vendor/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
 
 <!-- Vendor JS -->
-<script src="<?php echo base_url('vendor/onscreen/dist/on-screen.umd.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/onscreen/dist/on-screen.umd.min.js'); ?>"></script>
 
 <!-- Slider -->
-<script src="<?php echo base_url('vendor/nouislider/distribute/nouislider.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/nouislider/distribute/nouislider.min.js'); ?>"></script>
 
 <!-- Smooth scroll -->
-<script src="<?php echo base_url('vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js'); ?>"></script>
 
 <!-- Charts -->
-<script src="<?php echo base_url('vendor/chartist/dist/chartist.min.js');?>"></script>
-<script src="<?php echo base_url('vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/chartist/dist/chartist.min.js'); ?>"></script>
+<script src="<?php echo base_url('vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js'); ?>"></script>
 
 <!-- Datepicker -->
-<script src="<?php echo base_url('vendor/vanillajs-datepicker/dist/js/datepicker.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/vanillajs-datepicker/dist/js/datepicker.min.js'); ?>"></script>
 
 <!-- Sweet Alerts 2 -->
-<script src="<?php echo base_url('vendor/sweetalert2/dist/sweetalert2.all.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/sweetalert2/dist/sweetalert2.all.min.js'); ?>"></script>
 
 <!-- Moment JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
 
 <!-- Vanilla JS Datepicker -->
-<script src="<?php echo base_url('vendor/vanillajs-datepicker/dist/js/datepicker.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/vanillajs-datepicker/dist/js/datepicker.min.js'); ?>"></script>
 
 <!-- Notyf -->
-<script src="<?php echo base_url('vendor/notyf/notyf.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/notyf/notyf.min.js'); ?>"></script>
 
 <!-- Simplebar -->
-<script src="<?php echo base_url('vendor/simplebar/dist/simplebar.min.js');?>"></script>
+<script src="<?php echo base_url('vendor/simplebar/dist/simplebar.min.js'); ?>"></script>
 
 <!-- Github buttons -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 
 <!-- Volt JS -->
-<script src="<?php echo base_url('assets_admin/js/volt.js');?>"></script>
+<script src="<?php echo base_url('assets_admin/js/volt.js'); ?>"></script>
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
@@ -47,9 +47,10 @@
 
 <script>
   function deleteConfirm(url) {
+    console.log(url);
     swal({
         title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover this imaginary file!",
+        text: "Once deleted, you will not be able to recover this imaginary data!",
         icon: "warning",
         buttons: true,
         dangerMode: true,
@@ -60,11 +61,11 @@
             icon: "success",
           });
           window.location.href = url;
-      }
-    });
+        }
+      });
   }
 </script>
 
 <script>
-  var base_url = '<?php echo base_url(); ?>';  
+  var base_url = '<?php echo base_url(); ?>';
 </script>
